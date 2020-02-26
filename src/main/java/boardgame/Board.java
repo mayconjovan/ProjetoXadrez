02400f7f7b0aaf6ac15688fa150d2ecf625930a9
Maycon J Pereira <mayconjovan@gmail.com>
2020-02-25 23:11:47 -0300

    Implementing possible moves of Rook

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -4,12 +4,11 @@ public class Board {
 
     private int rows;
     private int columns;
-
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         if (rows < 1 || columns < 1) {
-            throw new BoardException("Error creating coard: there must be at leat 1row and 1 column.");
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         }
         this.rows = rows;
         this.columns = columns;
@@ -26,14 +25,14 @@ public class Board {
 
     public Piece piece(int row, int column) {
         if (!positionExists(row, column)) {
-            throw new BoardException("Position not on the board.");
+            throw new BoardException("Position not on the board");
         }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position not on the board.");
+            throw new BoardException("Position not on the board");
         }
         return pieces[position.getRow()][position.getColumn()];
     }
@@ -45,7 +44,7 @@ public class Board {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
-    
+
     public Piece removePiece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
@@ -56,11 +55,11 @@ public class Board {
         Piece aux = piece(position);
         aux.position = null;
         pieces[position.getRow()][position.getColumn()] = null;
-        return aux;        
+        return aux;
     }
 
     private boolean positionExists(int row, int column) {
-        return row >= 0 && row < rows && columns >= 0 && column < columns;
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
     public boolean positionExists(Position position) {
@@ -69,7 +68,7 @@ public class Board {
 
     public boolean thereIsAPiece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position not on the board.");
+            throw new BoardException("Position not on the board");
         }
         return piece(position) != null;
     }
