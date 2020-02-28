d98d1f30dc00437e25839429213978fdac96bc5d
Maycon J Pereira <mayconjovan@gmail.com>
2020-02-28 19:21:47 -0300

    Check logic

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -35,6 +35,7 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+                
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
